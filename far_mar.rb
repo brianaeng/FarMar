a73e6a53937b13aa53b5484f012f022e5b0433ff
Briana Eng <brianaeng@gmail.com>
2016-09-06 12:43:54 -0700

    Updated module file with requires

--- far_mar.rb
+++ far_mar.rb
@@ -1,10 +1,10 @@
 require 'csv'
-require 'lib/market'
-require 'lib/product'
-require 'lib/sale'
-require 'lib/vendor'
-
 
 module FarMar
 
 end
+
+require_relative 'lib/market'
+require_relative 'lib/product'
+require_relative 'lib/sale'
+require_relative 'lib/vendor'
