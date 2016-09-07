ab58e32ef803a94e47eba14ce69e5136c362f1a5
Briana Eng <brianaeng@gmail.com>
2016-09-07 15:03:48 -0700

    Added some test runs and required classes

--- far_mar.rb
+++ far_mar.rb
@@ -1,13 +1,32 @@
 require 'csv'
 require 'awesome_print'
 
-module FarMar
+module FarMar; end
 
-end
+require_relative 'lib/market'
+require_relative 'lib/product'
+require_relative 'lib/sale'
+require_relative 'lib/vendor'
 
-# require_relative 'lib/market'
-# require_relative 'lib/product'
-# require_relative 'lib/sale'
-# require_relative 'lib/vendor'
+# #MARKET INSTANCE METHOD
+# testing1 = FarMar::Market.all
+# ap testing1[492].vendors
+#
+# #VENDOR INSTANCE METHODS
+# testing2 = FarMar::Vendor.all
+# ap testing2[7].market
+#
+# testing3 = FarMar::Vendor.all
+# ap testing3[492].products
+#
+# testing4 = FarMar::Vendor.all
+# ap testing4[13].sales
+#
+# testing5 = FarMar::Vendor.all
+# ap testing5[0].revenue
+#
+# ap FarMar::Vendor.by_market(1)
 
-#If I use the above it will run twice
+#PRODUCT INSTANCE METHODS
+testing6 = FarMar::Product.all
+ap testing6[166].vendor
