fe63a1a2f70d198707653b7203505a78f19a28bf
Briana Eng <brianaeng@gmail.com>
2016-09-09 14:55:29 -0700

    Added more test runs for methods

--- far_mar.rb
+++ far_mar.rb
@@ -8,11 +8,20 @@ require_relative 'lib/product'
 require_relative 'lib/sale'
 require_relative 'lib/vendor'
 
-# #MARKET INSTANCE METHOD
+# #MARKET METHODS
+# ap FarMar::Market.find(493)
+#
 # testing1 = FarMar::Market.all
 # ap testing1[492].vendors
+# ap testing1[492].products
+# ap testing1[2].prefered_vendor
+# ap testing1[2].worst_vendor
 #
-# #VENDOR INSTANCE METHODS
+# ap FarMar::Market.search('Stamm')
+#
+# #VENDOR METHODS
+# ap FarMar::Vendor.find(40)
+#
 # testing2 = FarMar::Vendor.all
 # ap testing2[7].market
 #
@@ -27,10 +36,23 @@ require_relative 'lib/vendor'
 #
 # ap FarMar::Vendor.by_market(43)
 #
-# #PRODUCT INSTANCE METHODS
+# ap FarMar::Vendor.most_revenue(34)
+#
+# #PRODUCT METHODS
+# ap FarMar::Product.find(2432)
+#
 # testing6 = FarMar::Product.all
 # ap testing6[166].vendor
+# ap testing6[166].sales
+# ap testing6[166].number_of_sales
 #
-# # SALE INSTANCE METHODS
-# FarMar::Sale.all
+# ap FarMar::Product.by_vendor
+#
+# # SALE METHODS
+# ap FarMar::Sale.find(424)
+#
+# testing7 = FarMar::Sale.all
+#
+# ap testing7[166].product
+#
 # ap FarMar::Sale.between('2013-11-08 04:31:41 -0800', '2013-11-08 05:19:05 -0800')
